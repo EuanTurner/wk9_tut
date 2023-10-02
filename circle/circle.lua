0190f4232b66e9cf1f72d94a5aaaf1c953a673a0
Euan Turner <eturn66@hotmail.com>
2023-10-02 11:42:09 +1000

    circle completed

--- circle/circle.lua
+++ circle/circle.lua
@@ -12,8 +12,6 @@ arc2 = Arc:new{p0=southPoint,   p1=eastPoint,   centre=c}
 arc3 = Arc:new{p0=southPoint,    p1=westPoint,  centre=c}
 arc4 = Arc:new{p0=eastPoint,    p1=northPoint,  centre=c}
 
--- circle = Polyline:new{segments={arc1, arc2, arc3, arc4}}
-
 patch0 = CoonsPatch:new{south=arc2, north=arc1, west=arc3, east=arc4}
 grid0 = StructuredGrid:new{psurface=patch0, niv=21, njv=11}
 grid0:write_to_vtk_file('circle.vtk')
